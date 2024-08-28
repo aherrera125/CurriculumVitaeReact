2a1022e2017409982fb14d7ec97f4d4352e15364
aherrera125 <j.alejandroherrera@gmail.com>
2024-08-28 10:55:18 +0200

    Import React

--- index.jsx
+++ index.jsx
@@ -1,4 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from "./app";
+
+/*const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<React.StrictMode><App /></React.StrictMode>);*/
+
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 root.render(
